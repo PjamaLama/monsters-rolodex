44bbe048cca2f8d8937c6cdfc49ad3f17af265d6
Marnu Van Niekerk <Marnu.VanNiekerk@derivco.co.za>
2020-01-17 06:41:22 +0200

    Updates

---
+++ precache-manifest.e6f7170b9bcb08f64a74c533b2e088c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e23fc47442dd8a181b67f76ff6af83",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9419c135607e35b060c0",
+    "url": "/monsters-rolodex/static/css/main.337773f9.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9419c135607e35b060c0",
+    "url": "/monsters-rolodex/static/js/main.e6310a07.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
